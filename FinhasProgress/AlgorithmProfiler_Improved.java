d84c1f36508e6a7705e607976fa9d2b9c4b11484
Fellasfaw <fellasfaw@outlook.com>
2023-08-09 11:12:04 +0300

    Squashed commit of the following:
    commit de22765c91c31a3a282937e95229d5810cc3ad9e
    Author: Fellasfaw <fellasfaw@outlook.com>
    Date:   Wed Aug 9 11:03:33 2023 +0300
    
        Resolved runtime errors and successfuly ran the program.
    
    commit e40c8c419560f0927c144e59f390ba97695db110
    Author: Fellasfaw <fellasfaw@outlook.com>
    Date:   Wed Aug 9 11:01:03 2023 +0300
    
        Added a .gitignore file.
    
    commit 5960535dcf35a398ac61ca52fa5bd12e8fca4efa
    Author: Fellasfaw <fellasfaw@outlook.com>
    Date:   Wed Aug 9 10:15:30 2023 +0300
    
        Relocated files to FinhasProgress.
    
    commit 7061e8f689a604a5ecff29e1beed59fd98b651b6
    Author: Fellasfaw <fellasfaw@outlook.com>
    Date:   Wed Aug 9 10:15:09 2023 +0300
    
        Resolved compile time errors for AlgorithmProfiler_Improved.
    
    commit c31f9fec6075c2ada70db57207fb57278b3db5d3
    Author: Fellasfaw <fellasfaw@outlook.com>
    Date:   Wed Aug 9 09:53:55 2023 +0300
    
        Resolved compile time errors in SelectionSorter and shortened the code.
    
    commit bc049a535d6b68d2feca6247bf5fdfebd382a873
    Author: Zelalem Amare <fellasfaw@outlook.com>
    Date:   Tue Aug 8 21:51:26 2023 +0300
    
        Added best, average and worst case testing code for selection sorting + a function for neatly printing an arraylist.
    
    commit 863a8eb255443167e57d2e152f7cabaa14578dc2
    Author: Zelalem Amare <fellasfaw@outlook.com>
    Date:   Tue Aug 8 21:45:34 2023 +0300
    
        Added a function for sorting in descending order to SelectionSorter.
    
    commit a89d138c1f4345739e93e560770def386ac8372b
    Author: Zelalem Amare <fellasfaw@outlook.com>
    Date:   Tue Aug 8 18:49:53 2023 +0300
    
        Added a new AlgorithmProfiler class with a dataSetGenerator + a hashmap printer + a new main function.
    
    commit 2105b5718af16e514f03a4864455cf764686b987
    Author: Zelalem Amare <fellasfaw@outlook.com>
    Date:   Tue Aug 8 18:36:26 2023 +0300
    
        Added SelectionSorter class with sorting and timing algorithm.

---
+++ FinhasProgress/AlgorithmProfiler_Improved.java
@@ -0,0 +1,140 @@
+package FinhasProgress;
+//Standard libraries
+//import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlgorithmProfiler_Improved{
+
+    public static void main (String...str){
+        // Hash maps to hold the problem size vs time duration for the three cases in the selection sort algorithm.
+        HashMap<Integer, Long> selection_size_duration_map_best = new HashMap<>();
+        HashMap<Integer, Long> selection_size_duration_map_average = new HashMap<>();
+        HashMap<Integer, Long> selection_size_duration_map_worst = new HashMap<>();
+        
+        // Hash maps to hold the problem size vs time duration for the three cases in the bubble sort algorithm.
+
+        // Hash maps to hold the problem size vs time duration for the three cases in the insertion sort algorithm.
+
+        //===============================================================
+        //CODE FOR TESTING OUT THE ALGORITHMS
+        //===============================================================
+        
+        SelectionSorter selectionSorter;
+        //BubbleSorter bubbleSorter;
+        //InsertionSorter insertionSorter;
+
+        ArrayList<Double> dataSet;
+        for(int i=1; i<30; i+=2){
+
+            //Array list for holding the current randomly generated data set
+            //to compare the three algorithms with the same data set.
+            dataSet = AlgorithmProfiler_Improved.dataSetGenerator(i);
+
+            //---------------------------------------------------------------
+            //Code for testing the selection sort algorithm in SelectionSorter
+            //---------------------------------------------------------------
+            selectionSorter = new SelectionSorter(dataSet);
+            
+            //On Best case -> on an already sorted array.
+            selectionSorter.sortAscending();
+            ArrayList<Double> alreadySorted = selectionSorter.dataSet;
+            selectionSorter = new SelectionSorter(alreadySorted);
+            selectionSorter.sortAscending();
+            selection_size_duration_map_best.put(i, selectionSorter.time_taken);
+            
+            //On Average case
+            selectionSorter = new SelectionSorter(dataSet);
+            selectionSorter.sortAscending();
+            selection_size_duration_map_average.put(i, selectionSorter.time_taken);
+
+            //On Worst case
+            selectionSorter.sortDescending();
+            ArrayList<Double> reversed = selectionSorter.dataSet;
+            selectionSorter = new SelectionSorter(reversed);
+            selectionSorter.sortAscending();
+            selection_size_duration_map_worst.put(i, selectionSorter.time_taken);
+
+            //---------------------------------------------------------------
+            //Code for testing the selection sort algorithm in BubbleSorter
+            //---------------------------------------------------------------
+           
+            
+            //---------------------------------------------------------------
+            //Code for testing the selection sort algorithm in InsertionSorter
+            //---------------------------------------------------------------
+            
+        }
+
+        //===============================================================
+        //CODE FOR DISPLAYING RESULTS
+        //===============================================================
+        //---------------------------------------------------------------
+        //Code for displaying the results of selection sort algorithm
+        //---------------------------------------------------------------
+        {
+            System.out.println("\n\nRESULTS ON THE SELECTION SORT ALGORITHM:");
+            
+            //On best case
+            System.out.println("\nFor best case:");
+            printHashMap(selection_size_duration_map_best);
+            //On average case
+            System.out.println("\nFor average case:");
+            printHashMap(selection_size_duration_map_average);
+            //On worst case
+            System.out.println("\nFor worst case:");
+            printHashMap(selection_size_duration_map_worst);
+
+        }
+        //---------------------------------------------------------------
+        //Code for displaying the results of bubble sort algorithm
+        //---------------------------------------------------------------
+        
+
+        //---------------------------------------------------------------
+        //Code for displaying the results of insertion sort algorithm
+        //---------------------------------------------------------------
+    }
+
+    //A function that generates as many random numbers as needed.
+    public static ArrayList<Double> dataSetGenerator(int quantity){
+        //Array list that will contain all the random numbers
+        ArrayList<Double> to_be_returned = new ArrayList<Double>();
+        
+        //Random number object for generating random numbers
+        Random random_number = new Random();
+        
+        //Add as many random numbers to the list as is specified by 'quantity'
+        for(int i=0; i<quantity; i++){
+            to_be_returned.add(random_number.nextDouble());
+        }
+
+        return to_be_returned;
+    }
+
+    public static void printHashMap(HashMap<Integer, Long> table){
+        System.out.println("[Data size, Time taken]");
+
+        for(Map.Entry<Integer, Long> record : table.entrySet()){
+            int data_size = record.getKey();
+            long time_taken = record.getValue();
+
+            System.out.printf("[%d, %d] \n", data_size, time_taken);
+        }
+    }
+
+    public static String printDataSet(ArrayList<Double> dataSet){
+        String to_be_printed = "{";
+        int len = dataSet.size();
+        for(int i=0; i<len; i++){
+            to_be_printed += String.valueOf(dataSet.get(i));
+            
+            if(i != len-1) to_be_printed += ", ";
+        }
+        to_be_printed += "}";
+
+        return to_be_printed;
+    }
+}
