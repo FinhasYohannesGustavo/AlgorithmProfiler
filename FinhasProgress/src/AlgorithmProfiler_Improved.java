336692b74d8e9305bdb88f617a814e3209cac887
Zelalem Amare <fellasfaw@outlook.com>
2023-08-11 17:30:16 +0300

    Final touches on AlgorithmProfiler.

--- FinhasProgress/src/AlgorithmProfiler_Improved.java
+++ FinhasProgress/src/AlgorithmProfiler_Improved.java
@@ -1,6 +1,4 @@
 package FinhasProgress.src;
-//Standard libraries
-//import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -10,88 +8,118 @@ import java.util.Map;
 public class AlgorithmProfiler_Improved{
 
     public static void main (String...str){
-        // Hash maps to hold the problem size vs time duration for the three cases in the selection sort algorithm.
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        
+        //CODE FOR TESTING OUT THE ALGORITHMS
+        
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+        // Hash maps to hold the problem size vs time duration mapping for the three cases in the selection sort algorithm.
         HashMap<Integer, Long> selection_size_duration_map_best = new HashMap<>();
         HashMap<Integer, Long> selection_size_duration_map_average = new HashMap<>();
         HashMap<Integer, Long> selection_size_duration_map_worst = new HashMap<>();
         
-        // Hash maps to hold the problem size vs time duration for the three cases in the bubble sort algorithm.
+        // Hash maps to hold the problem size vs time duration mapping for the three cases in the bubble sort algorithm.
+        HashMap<Integer, Long> bubble_size_duration_map_best = new HashMap<>();
+        HashMap<Integer, Long> bubble_size_duration_map_average = new HashMap<>();
+        HashMap<Integer, Long> bubble_size_duration_map_worst = new HashMap<>();
 
-        // Hash maps to hold the problem size vs time duration for the three cases in the insertion sort algorithm.
-         HashMap<Integer, Long> insertion_size_duration_map_best = new HashMap<>();
+        // Hash maps to hold the problem size vs time duration mapping for the three cases in the insertion sort algorithm.
+        HashMap<Integer, Long> insertion_size_duration_map_best = new HashMap<>();
         HashMap<Integer, Long> insertion_size_duration_map_average = new HashMap<>();
         HashMap<Integer, Long> insertion_size_duration_map_worst = new HashMap<>();
-
-        //===============================================================
-        //CODE FOR TESTING OUT THE ALGORITHMS
-        //===============================================================
         
+        // Variables for accessing the specific sorting algorithm type.
         SelectionSorter selectionSorter;
-        //BubbleSorter bubbleSorter;
+        BubbleSorter bubbleSorter;
         InsertionSorter insertionSorter;
 
-        //ArrayList<Integer> dataSet;
-        for(int i=1; i<41; i++){
+        ArrayList<Integer> dataSet;
+        for(int i=10; i<400; i+=10){
 
             //Array list for holding the current randomly generated data set
             //to compare the three algorithms with the same data set.
-            ArrayList<Integer> dataSet = AlgorithmProfiler_Improved.dataSetGenerator(i*1000);
+            dataSet = AlgorithmProfiler_Improved.dataSetGenerator(i);
 
             //---------------------------------------------------------------
             //Code for testing the selection sort algorithm in SelectionSorter
             //---------------------------------------------------------------
+            {
+                selectionSorter = new SelectionSorter(dataSet);
+                
+                //On Average case -> jumbled data set.
+                selectionSorter.sortAscending();
+                selection_size_duration_map_average.put(i, selectionSorter.time_taken); 
+                
+                //On Best case -> on an already sorted data set.
+                Collections.sort(dataSet);
+                selectionSorter.dataSet = dataSet;
+                selectionSorter.sortAscending();
+                selection_size_duration_map_best.put(i, selectionSorter.time_taken); 
+    
+                //On Worst case -> reverese-sorted data set.
+                Collections.sort(dataSet, Collections.reverseOrder());
+                selectionSorter.dataSet = dataSet;
+                selectionSorter.sortAscending();
+                selection_size_duration_map_worst.put(i, selectionSorter.time_taken); 
+            }
             
-            //On Average case
-            selectionSorter = new SelectionSorter(dataSet);
-            selectionSorter.sortAscending();
-            selection_size_duration_map_average.put(i, selectionSorter.time_taken);
-            
-            //On Best case -> on an already sorted array.
-            Collections.sort(dataSet);
-            selectionSorter = new SelectionSorter(dataSet);
-            selectionSorter.sortAscending();
-            selection_size_duration_map_best.put(i, selectionSorter.time_taken);
-            
-            //On Worst case
-            Collections.sort(dataSet, Collections.reverseOrder());
-            selectionSorter = new SelectionSorter(dataSet);
-            selectionSorter.sortAscending();
-            selection_size_duration_map_worst.put(i, selectionSorter.time_taken);
- 
             //---------------------------------------------------------------
             //Code for testing the bubble sort algorithm in BubbleSorter
             //---------------------------------------------------------------
+            {
+                bubbleSorter = new BubbleSorter(dataSet);
+                
+                //On Average case -> jumbled data set.
+                bubbleSorter.sortAscending();
+                bubble_size_duration_map_average.put(i, bubbleSorter.time_taken); 
+                
+                //On Best case -> on an already sorted data set.
+                Collections.sort(dataSet);
+                bubbleSorter.dataSet = dataSet;
+                bubbleSorter.sortAscending();
+                bubble_size_duration_map_best.put(i, bubbleSorter.time_taken); 
+    
+                //On Worst case -> reverese-sorted data set.
+                Collections.sort(dataSet, Collections.reverseOrder());
+                bubbleSorter.dataSet = dataSet;
+                bubbleSorter.sortAscending();
+                bubble_size_duration_map_worst.put(i, bubbleSorter.time_taken); 
+            }
            
-            
             //---------------------------------------------------------------
             //Code for testing the selection sort algorithm in InsertionSorter
             //---------------------------------------------------------------
-            insertionSorter = new InsertionSorter(dataSet);
-            
-            //On Best case -> on an already sorted array.
-            insertionSorter.sortAscending();
-            ArrayList<Integer> alreadySortedInsertion = insertionSorter.dataSet;
-            insertionSorter = new InsertionSorter(alreadySortedInsertion);
-            insertionSorter.sortAscending();
-            insertion_size_duration_map_best.put(i, insertionSorter.time_taken); 
-            
-            //On Average case
-            insertionSorter = new InsertionSorter(dataSet);
-            insertionSorter.sortAscending();
-            insertion_size_duration_map_average.put(i, insertionSorter.time_taken); 
-
-            //On Worst case
-           
-            
-            Collections.sort(dataSet, Collections.reverseOrder());
-            insertionSorter = new InsertionSorter(dataSet);
-            insertionSorter.sortAscending();
-            insertion_size_duration_map_worst.put(i, insertionSorter.time_taken); 
+            {
+                insertionSorter = new InsertionSorter(dataSet);
+                
+                //On Average case -> jumbled data set.
+                insertionSorter.sortAscending();
+                insertion_size_duration_map_average.put(i, insertionSorter.time_taken); 
+                
+                //On Best case -> on an already sorted data set.
+                Collections.sort(dataSet);
+                insertionSorter.dataSet = dataSet;
+                insertionSorter.sortAscending();
+                insertion_size_duration_map_best.put(i, insertionSorter.time_taken); 
+    
+                //On Worst case -> reverese-sorted data set.
+                Collections.sort(dataSet, Collections.reverseOrder());
+                insertionSorter.dataSet = dataSet;
+                insertionSorter.sortAscending();
+                insertion_size_duration_map_worst.put(i, insertionSorter.time_taken); 
+            }
         }
 
-        //===============================================================
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        
         //CODE FOR DISPLAYING RESULTS
-        //===============================================================
+        
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
         //---------------------------------------------------------------
         //Code for displaying the results of selection sort algorithm
         //---------------------------------------------------------------
@@ -113,10 +141,38 @@ public class AlgorithmProfiler_Improved{
         //Code for displaying the results of bubble sort algorithm
         //---------------------------------------------------------------
         
+        {
+           System.out.println("\n\nRESULTS ON THE BUBBLE SORT ALGORITHM:");
+           
+           //On best case
+           System.out.println("\nFor best case:");
+           printHashMap(bubble_size_duration_map_best);
+           //On average case
+           System.out.println("\nFor average case:");
+           printHashMap(bubble_size_duration_map_average);
+           //On worst case
+           System.out.println("\nFor worst case:");
+           printHashMap(bubble_size_duration_map_worst);
 
+        } 
+       
         //---------------------------------------------------------------
         //Code for displaying the results of insertion sort algorithm
         //---------------------------------------------------------------
+        {
+           System.out.println("\n\nRESULTS ON THE INSERTION SORT ALGORITHM:");
+           
+           //On best case
+           System.out.println("\nFor best case:");
+           printHashMap(insertion_size_duration_map_best);
+           //On average case
+           System.out.println("\nFor average case:");
+           printHashMap(insertion_size_duration_map_average);
+           //On worst case
+           System.out.println("\nFor worst case:");
+           printHashMap(insertion_size_duration_map_worst);
+ 
+        } 
     }
 
     //A function that generates as many random numbers as needed.
@@ -135,6 +191,7 @@ public class AlgorithmProfiler_Improved{
         return to_be_returned;
     }
 
+    //A function that prints a hashmap.
     public static void printHashMap(HashMap<Integer, Long> table){
         System.out.println("[Data size, Time taken in microseconds]");
 
@@ -143,10 +200,10 @@ public class AlgorithmProfiler_Improved{
             long time_taken = record.getValue();
 
             System.out.printf("[%d, %d] \n", data_size, time_taken);
-            
         }
     }
 
+    //A function that prints an arraylist of type Integer. Used for testing purposes.
     public static String printDataSet(ArrayList<Integer> dataSet){
         String to_be_printed = "{";
         int len = dataSet.size();
