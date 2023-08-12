f317f01f4a1d8b8f6b058b79d443d90bf32012c1
Fellasfaw <fellasfaw@outlook.com>
2023-08-12 14:02:49 +0300

    Added DataSorter as a subclass of AlgorithmProfiler.

--- FinhasProgress/src/AlgorithmProfiler.java
+++ FinhasProgress/src/AlgorithmProfiler.java
@@ -7,6 +7,104 @@ import java.util.Map;
 
 public class AlgorithmProfiler{
 
+    //A class that holds all the algorithm types.
+    //It has to be static to avoid creating an instance of AlgorithmProfiler to access its functions.
+    static class DataSorter {
+        //Field for holding the data set to be sorted.
+        ArrayList<Integer> dataSet;
+    
+        //Variable for holding how long the sorting took.
+        long time_taken;
+
+        public DataSorter(ArrayList<Integer> dataSet){
+            this.dataSet = dataSet;
+        }
+    
+        public void selectionSort(){
+            //Variable for holding the start time.
+            long start_time = System.nanoTime();
+            
+            //Variable for holding the size of the array list.
+            int len = dataSet.size();
+            //Variable for holding the index of the current minimum portion.
+            int index_of_min = 0;
+    
+            for (int i=0; i<len-1; i++){
+                index_of_min = i;
+    
+                //Look for the index of the minimum number in the unsorted part of the data set.
+                for (int j=i+1; j<len; j++){
+                    if(dataSet.get(j) < dataSet.get(index_of_min)){
+                        index_of_min = j;
+                    }
+                }
+    
+                //Only switch if the minimum index has changed.
+                if(index_of_min != i){
+                    Collections.swap(dataSet, i, index_of_min);
+                }
+                    
+            }
+    
+            //Variable for holding the end time.
+            long end_time = System.nanoTime();
+            //Calculate the duration and set it to time_taken in microseconds.
+            time_taken = (end_time - start_time)/1000;
+        }
+    
+        public void bubbleSort(){
+            long start_time = System.nanoTime();
+            boolean swapped = true;
+            int size = dataSet.size();
+            int temp = 0;
+    
+            for (int i = 0; i < size - 1 && swapped; i++) {
+                for (int j = size - 1; j > i; j--) {
+                    swapped = false;
+                    
+                    if (dataSet.get(j) < dataSet.get(j - 1)) {
+                        temp = dataSet.get(j);
+                        // dataSet.set(j, temp );
+                        dataSet.set(j, dataSet.get(j - 1));
+                        dataSet.set(j - 1, temp);
+                        swapped = true;
+                    }
+                }
+            }
+    
+            //Variable for holding the end time.
+            long end_time = System.nanoTime();
+            
+            // Calculate the duration and set it to time_taken.
+            time_taken = (end_time - start_time)/1000;
+        }
+    
+        public void insertionSort(){
+            //Variable for holding the start time.
+            long start_time = System.nanoTime();
+            //Variable for holding the size of the array list.
+            int size = dataSet.size();
+        
+            for(int i = 1; i<size;i++){
+                
+                int temp = dataSet.get(i);
+                int j=i;
+                while(j>0&&temp<dataSet.get(j-1)){
+                    dataSet.set(j,dataSet.get(j-1)); //This sorting algorithm implements shifting not swapping
+                    j--;
+    
+                }
+                dataSet.set(j,temp);
+            }
+    
+            //Variable for holding the end time.
+            long end_time = System.nanoTime();
+            //Calculate the duration and set it to time_taken.
+            time_taken = (end_time - start_time)/1000;//time taken will be in microseconds
+    
+        }
+    }
+
     public static void main (String...str){
         //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
         //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -45,7 +143,6 @@ public class AlgorithmProfiler{
             //to compare the three algorithms with the same data set.
             dataSetRandom = AlgorithmProfiler.dataSetGenerator(i);
             dataSet = dataSetRandom;
-
             //---------------------------------------------------------------
             //Code for testing the | selection | sort algorithm in DataSorter
             //---------------------------------------------------------------
@@ -68,7 +165,6 @@ public class AlgorithmProfiler{
                 dataSorter.selectionSort();
                 selection_size_duration_map_worst.put(i, dataSorter.time_taken); 
             }
-            /* 
             //---------------------------------------------------------------
             //Code for testing the | bubble | sort algorithm in DataSorter
             //---------------------------------------------------------------
@@ -84,14 +180,14 @@ public class AlgorithmProfiler{
                 dataSorter.dataSet = dataSet;
                 dataSorter.bubbleSort();
                 bubble_size_duration_map_best.put(i, dataSorter.time_taken); 
-    
+                
                 //On Worst case -> reverese-sorted data set.
                 Collections.sort(dataSet, Collections.reverseOrder());
                 dataSorter.dataSet = dataSet;
                 dataSorter.bubbleSort();
                 bubble_size_duration_map_worst.put(i, dataSorter.time_taken); 
             }
-           
+            
             //---------------------------------------------------------------
             //Code for testing the | insertion | sort algorithm in DataSorter
             //---------------------------------------------------------------
@@ -114,7 +210,6 @@ public class AlgorithmProfiler{
                 dataSorter.insertionSort();
                 insertion_size_duration_map_worst.put(i, dataSorter.time_taken); 
             }
-            */
         }
 
         //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -204,7 +299,8 @@ public class AlgorithmProfiler{
             int data_size = record.getKey();
             long time_taken = record.getValue();
 
-            System.out.printf("[%d, %d]\n", data_size, time_taken);
+            // System.out.printf("[%d, %d]\n", data_size, time_taken);
+            System.out.println(time_taken);
         }
     }
 
